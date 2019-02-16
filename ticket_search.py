ebe6cebc7f06d03e888a58b43091ee9a6232f0fe
kaktakdavottak <semenmineev@gmail.com>
2019-02-16 16:58:31 +0300

    Initial commit

---
+++ ticket_search.py
@@ -0,0 +1,34 @@
+import csv
+import re
+
+from pymongo import MongoClient
+
+
+
+def read_data(csv_file, db):
+    """
+    Загрузить данные в бд из CSV-файла
+    """
+    with open(csv_file, encoding='utf8') as csvfile:
+        # прочитать файл с данными и записать в коллекцию
+        reader = csv.DictReader(csvfile)
+
+
+def find_cheapest(db):
+    """
+    Найти самые дешевые билеты
+    Документация: https://docs.mongodb.com/manual/reference/operator/aggregation/sort/
+    """
+
+
+def find_by_name(name, db):
+    """
+    Найти билеты по имени исполнителя (в том числе – по подстроке),
+    и выведите их по возрастанию цены
+    """
+
+    regex = re.compile('укажите регулярное выражение для поиска')
+
+
+if __name__ == '__main__':
+    pass
